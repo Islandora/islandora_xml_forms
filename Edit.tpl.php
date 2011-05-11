27ddf370edb45e5b7e8897c7afc19d85065a22a7
Nigel Banks <nigel.g.banks@gmail.com>
2011-05-11 19:28:15 -0300

    Start on Edit page, changed paths so the builder lives in the admin's content section.

---
+++ Edit.tpl.php
@@ -0,0 +1,25 @@
+<?php ?>
+<div id="xml-form-builder-edit-page">
+  <!-- Tree -->
+  <div id="xml-form-builder-edit-tree">
+    <ul>
+      <li id="one">
+        <a href="#">Root Node</a>
+      </li>
+      <li id="two">
+        <a href="#">Root Node 2</a>
+      </li>
+    </ul>
+  </div>
+  <!-- Display Area forms/previews appear here -->
+  <div id="xml-form-builder-edit-display">
+    <!-- Element Form -->
+    <div id="xml-form-builder-edit-form">
+      <?php print $element_form; ?>
+    </div>
+    <!-- Element Form -->
+    <div id="xml-form-builder-edit-preview">
+
+    </div>
+  </div>
+</div>
---
+++ js/EditForm.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// 
+$(document).ready(function() {
+   jQuery('#xml-form-builder-edit-tree').jstree({ "plugins" : [ "themes", "html_data" ] }); 
+});
