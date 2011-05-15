e1093b56ca8635931e6938ccf3039782dbc5aa32
Nigel Banks <nigel.g.banks@gmail.com>
2011-05-16 05:28:25 +0900

    Editor, clearing up some css problems. Added toolbars.

--- js/EditForm.js
+++ js/EditForm.js
@@ -13,7 +13,34 @@ Ext.app = (function() {
                 title: 'Form Editor',
                 layout: 'border',
                 renderTo: 'xml-form-builder-editor',
-                items: [this.createTree(), this.createForm() ]
+                items: [this.createTree(), this.createForm() ],
+                defaults: {
+                  margin: '1 0 1 0',
+                  frame: true
+                },
+                tbar: {
+                    xtype: 'toolbar',
+                    items: [
+                    {
+                        xtype: 'button',
+                        text: 'Form Properties'
+                    },
+                    {
+                        xtype: 'tbfill'
+                    },
+                    {
+                        xtype: 'button',
+                        text: 'Save'
+                    },
+                    {
+                        xtype: 'tbseparator'
+                    },
+                    {
+                        xtype: 'button',
+                        text: 'Save & Preview'
+                    }
+                    ]
+                }
             });
         },
         createTree: function () {
@@ -26,15 +53,43 @@ Ext.app = (function() {
                 title: 'Elements',
                 store: this.createElementStore(),
                 region: 'west',
-                width: 300,
+                width: 230,
+                margin: '1 0 1 1',
                 autoScroll: true,
-                rootVisible: true
+                rootVisible: true,
+                split: true,
+                tbar: {
+                    xtype: 'toolbar',
+                    items: [
+                    {
+                        xtype: 'button',
+                        text: 'Add'
+                    },
+                    {
+                        xtype: 'button',
+                        text: 'Edit'
+                    },
+                    {
+                        xtype: 'button',
+                        text: 'Copy'
+                    },
+                    {
+                        xtype: 'button',
+                        text: 'Paste'
+                    },
+                    {
+                        xtype: 'button',
+                        text: 'Delete'
+                    }
+                    ]
+                }
             });
         },
         createForm: function() {
             return Ext.create('Ext.form.Panel', {
                 title: 'Element',
                 region: 'center',
+                margin: '1 1 1 0',
                 items: [{
                     xtype: 'textfield',
                     name: 'name',
