dbdc9ae58221ed1a290dbe119d6a27d1248ddd78
Nigel Banks <nigel.g.banks@gmail.com>
2011-05-16 04:53:27 +0900

    Stubbing in some work for the form builder, that was developed outside of drupal.

--- js/EditForm.js
+++ js/EditForm.js
@@ -2,8 +2,62 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+Ext.ns('Ext.app');
 
-// 
-$(document).ready(function() {
-   
+Ext.app = (function() {
+    var that = {
+        create: function () {
+            return Ext.create('Ext.panel.Panel', {
+                width: 960,
+                height: 800,
+                title: 'Form Editor',
+                layout: 'border',
+                renderTo: 'xml-form-builder-editor',
+                items: [this.createTree(), this.createForm() ]
+            });
+        },
+        createTree: function () {
+            return Ext.create('Ext.tree.Panel', {
+                viewConfig: {
+                    plugins: {
+                        ptype: 'treeviewdragdrop'
+                    }
+                },
+                title: 'Elements',
+                store: this.createElementStore(),
+                region: 'west',
+                width: 300,
+                autoScroll: true,
+                rootVisible: true
+            });
+        },
+        createForm: function() {
+            return Ext.create('Ext.form.Panel', {
+                title: 'Element',
+                region: 'center',
+                items: [{
+                    xtype: 'textfield',
+                    name: 'name',
+                    fieldLabel: 'Name',
+                    allowBlank: false
+                }]
+            });
+        },
+        createElementStore: function () {
+            return Ext.create('Ext.data.TreeStore', {
+                model: 'Element',
+                root: {
+                    text: 'Elements',
+                    root: 'elements',
+                    id: 'id'
+                }
+            });
+        }
+    }
+    return that;
+})();
+
+//
+Ext.onReady(function() {
+    var app = Ext.app.create();
 });
