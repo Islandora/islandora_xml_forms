cdbbc7553909923db0988860fe2ff2c87f991fc4
Nigel Banks <nigel.g.banks@gmail.com>
2011-06-07 09:39:03 -0300

    Work on the Tags element.

--- Tags.tpl.php
+++ Tags.tpl.php
@@ -1,14 +1,20 @@
 <div class="xml-form-elements-tags form-item">
-  <label for="<?php print "{$input['id']}" ?>"><?php print "{$label}" ?></label>
+  <label for="<?php print "{$input['#id']}" ?>"><?php print "{$label}" ?></label>
   <input type="text" 
-         name="<?php "{$input['name']}" ?>" 
-         id="<?php print "{$input['id']}" ?>" 
+         name="<?php "{$input['#name']}" ?>" 
+         id="<?php print "{$input['#id']}" ?>" 
          size="25" 
-         value="<?php print "{$input['value']}" ?>" 
-         class="form-text listElement list">
-  <div>
+         value="<?php print "{$input['#value']}" ?>" 
+         class="form-text tag-editor">
+  <div class="add-tag" style="display:inline-block"><input type="image" src="http://xml_form_api.local/sites/xml_form_api.local/modules/xml_form_elements/images/add.png" name="<?php print $add['#name'] ?>" id="<?php print $add['#id'] ?>" value="add"></div>
+  <div class="hidden-tags">
+    <?php foreach($tags as $tag): ?>
+      <input type="hidden" name="<?php print $tag['#name'] ?>" id="<?php print $tag['#id'] ?>" value="<?php print "{$tag['#value']}" ?>">
+    <?php endforeach ?>
   </div>
-  <ui class="tagEditor">
-    <li title="Remove tag">remove</li>
+  <ui class="tag-list">
+    <?php foreach($tags as $tag): ?>
+    <li title="<?php print "{$tag['#value']}" ?>"><span class="edit-tag"><?php print "{$tag['#value']}" ?></span><span class="remove-tag"></span></li>
+    <?php endforeach ?>
   </ui>
 </div>
--- js/Tabs.js
+++ js/Tabs.js
@@ -2,73 +2,71 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-xml_form_elements = {
-    tabpanel: {
-        tabs: null, // Collection of all tabpanels.
-        collapsibleTabs: null,
-        nonCollapsibleTabs: null,
-        loadPanels: function (collapse) {
-            var load = '.xml-form-elements-tabs';
-            var collapsible = '.xml-form-elements-tabs-collapsible';
-            var collapsed = '.xml-form-elements-tabs-collapsed';
-            this.tabs = $(load);
-            this.collapsibleTabs = this.tabs.filter(collapsible);
-            this.nonCollapsibleTabs = this.tabs.not(collapsible);
-            var expandedTabs = this.collapsibleTabs.not(collapsed);
-            var collapsedTabs = this.collapsibleTabs.filter(collapsed);
-            if(collapsedTabs.length > 0) {
-                collapsedTabs.tabs({
-                    collapsible: true,
-                    selected: collapse ? -1 : undefined,
-                    select: this.setCollapsibleIconOnSelect,
-                    create: this.setCollapsibleIconOnCreate
-                });
-            }
-            if(expandedTabs.length > 0) {
-                expandedTabs.tabs({
-                    collapsible: true,
-                    select: this.setCollapsibleIconOnSelect,
-                    create: this.setCollapsibleIconOnCreate
-                });
-            }
-            if(this.nonCollapsibleTabs.length > 0) {
-                this.nonCollapsibleTabs.tabs({});
-            }
-        },
-        setCollapsibleIconOnSelect: function(event, ui) {
-            var icon = $('span.expand-tabpanel-icon:first', this);
-            if($(ui.panel).hasClass('ui-tabs-hide')) {
-                icon.removeClass('ui-icon-circle-triangle-e');
-                icon.addClass('ui-icon-circle-triangle-s');
-            }
-            else {
-                icon.removeClass('ui-icon-circle-triangle-s');
-                icon.addClass('ui-icon-circle-triangle-e');
-            }
-        },
-        setCollapsibleIconOnCreate: function(event, ui) {
-            var icon = $('span.expand-tabpanel-icon:first', this);
-            if($('div.ui-tabs-panel:not(.ui-tabs-hide)', this).length > 0) {
-                icon.removeClass('ui-icon-circle-triangle-e');
-                icon.addClass('ui-icon-circle-triangle-s');
-            }
-            else {
-                icon.removeClass('ui-icon-circle-triangle-s');
-                icon.addClass('ui-icon-circle-triangle-e');
-            }
-        },
-        enableActions: function () {
-            $(".ui-icon-close").live("click", function() {
-                var id = $(this).text();
-                $("#"+id).trigger("mousedown");
+xml_form_elements.tabpanel = {
+    tabs: null, // Collection of all tabpanels.
+    collapsibleTabs: null,
+    nonCollapsibleTabs: null,
+    loadPanels: function (collapse) {
+        var load = '.xml-form-elements-tabs';
+        var collapsible = '.xml-form-elements-tabs-collapsible';
+        var collapsed = '.xml-form-elements-tabs-collapsed';
+        this.tabs = $(load);
+        this.collapsibleTabs = this.tabs.filter(collapsible);
+        this.nonCollapsibleTabs = this.tabs.not(collapsible);
+        var expandedTabs = this.collapsibleTabs.not(collapsed);
+        var collapsedTabs = this.collapsibleTabs.filter(collapsed);
+        if(collapsedTabs.length > 0) {
+            collapsedTabs.tabs({
+                collapsible: true,
+                selected: collapse ? -1 : undefined,
+                select: this.setCollapsibleIconOnSelect,
+                create: this.setCollapsibleIconOnCreate
             });
-        },
-        addTab: function(id) {
-            $('#' + id).trigger("mousedown");
-            return false;
         }
-    } 
-};
+        if(expandedTabs.length > 0) {
+            expandedTabs.tabs({
+                collapsible: true,
+                select: this.setCollapsibleIconOnSelect,
+                create: this.setCollapsibleIconOnCreate
+            });
+        }
+        if(this.nonCollapsibleTabs.length > 0) {
+            this.nonCollapsibleTabs.tabs({});
+        }
+    },
+    setCollapsibleIconOnSelect: function(event, ui) {
+        var icon = $('span.expand-tabpanel-icon:first', this);
+        if($(ui.panel).hasClass('ui-tabs-hide')) {
+            icon.removeClass('ui-icon-circle-triangle-e');
+            icon.addClass('ui-icon-circle-triangle-s');
+        }
+        else {
+            icon.removeClass('ui-icon-circle-triangle-s');
+            icon.addClass('ui-icon-circle-triangle-e');
+        }
+    },
+    setCollapsibleIconOnCreate: function(event, ui) {
+        var icon = $('span.expand-tabpanel-icon:first', this);
+        if($('div.ui-tabs-panel:not(.ui-tabs-hide)', this).length > 0) {
+            icon.removeClass('ui-icon-circle-triangle-e');
+            icon.addClass('ui-icon-circle-triangle-s');
+        }
+        else {
+            icon.removeClass('ui-icon-circle-triangle-s');
+            icon.addClass('ui-icon-circle-triangle-e');
+        }
+    },
+    enableActions: function () {
+        $(".ui-icon-close").live("click", function() {
+            var id = $(this).text();
+            $("#"+id).trigger("mousedown");
+        });
+    },
+    addTab: function(id) {
+        $('#' + id).trigger("mousedown");
+        return false;
+    }
+}; 
 
 /**
  * On Load, listen for ajax requests and attempt to regenerate any new tabs.
