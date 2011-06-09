6d99a6906e38ff2bf1241f92a08e03111fb6ba14
Nigel Banks <nigel.g.banks@gmail.com>
2011-06-09 12:13:16 -0300

    Seperated the Ahah::rebuild function into two, so that the form could be processed earliar and its values used. Tags can now be added via ahah.

--- Tags.tpl.php
+++ Tags.tpl.php
@@ -1,7 +1,7 @@
 <div class="xml-form-elements-tags form-item">
   <label for="<?php print "{$input['#id']}" ?>"><?php print "{$label}" ?></label>
   <input type="text" 
-         name="<?php "{$input['#name']}" ?>" 
+         name="<?php print "{$input['#name']}" ?>" 
          id="<?php print "{$input['#id']}" ?>" 
          size="25" 
          value="<?php print "{$input['#value']}" ?>" 
