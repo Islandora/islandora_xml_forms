e0fa008fc8e91a85f5c74fef94b74352a5cd2e6f
Nigel Banks <nigel.g.banks@gmail.com>
2011-06-09 15:26:20 -0300

    Fixedgit add -A! There was a strange bug were values in form fields were being overwritten. By the first letter of the neighboring elements value. This bug still persists, but I've removed the value from the neighboring button. so the bug is no longer apparent.

--- Tags.tpl.php
+++ Tags.tpl.php
@@ -8,13 +8,17 @@
          class="form-text tag-editor">
   <div class="add-tag" style="display:inline-block"><input type="image" src="<?php print $image_path ?>/add.png" name="<?php print $add['#name'] ?>" id="<?php print $add['#id'] ?>" value="add"></div>
   <div class="hidden-tags">
-    <?php foreach($tags as $tag): ?>
+    <?php foreach ($tags as $tag): ?>
       <input type="hidden" name="<?php print $tag['#name'] ?>" id="<?php print $tag['#id'] ?>" value="<?php print "{$tag['#value']}" ?>">
     <?php endforeach ?>
   </div>
   <ui class="tag-list">
-    <?php foreach($tags as $tag): ?>
-    <li title="<?php print "{$tag['#value']}" ?>"><span class="edit-tag"><?php print "{$tag['#value']}" ?></span><input type="image" src="<?php print $image_path ?>/minus_small.png" name="<?php print $tag['remove-tag']['#name'] ?>" id="<?php print $tag['remove-tag']['#id'] ?>" value="remove"></li>
+    <?php foreach ($tags as $tag): ?>
+      <li title="<?php print "{$tag['#value']}" ?>">
+        <span class="edit-tag" onclick="$('#<?php print $tag['edit-tag']['#id'] ?>').trigger('mousedown'); return false;"><?php print "{$tag['#value']}" ?></span>
+        <span class="remove-tag" onclick="$('#<?php print $tag['remove-tag']['#id'] ?>').trigger('mousedown'); return false;"></span>
+        <?php print $tag['#children']; ?>
+      </li>
     <?php endforeach ?>
   </ui>
 </div>
