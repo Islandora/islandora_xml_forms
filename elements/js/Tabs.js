c7d1f6acce3046466b84a509ce619bcc8f9cfbf6
Adam Vessey <adam@discoverygarden.ca>
2012-02-17 13:47:00 -0400

    Deal with nested tabs/tabpanels a little more gracefully.
    ... Also, list selected objects.

--- elements/js/Tabs.js
+++ elements/js/Tabs.js
@@ -58,72 +58,75 @@ Drupal.settings.xml_form_elements.tabpanel = {
       icon.addClass('ui-icon-circle-triangle-e');
     }
   },
-  attachToolTips: function() {
-    $('.tool_tip_trigger').each(function() {
-      var tip = Drupal.settings.xml_form_elements.tabpanel.tool_tip;
-      if (tip != null) {
-        tip.remove();
-      }
-      tip = $(document.createElement('span')).addClass('tool_tip');
+  attachToolTips: function() { //TODO:  Fix this; nested stuff needs to work better...
+    $('.tool_tip_trigger').each(function(i, eel) {
       $(this).hover(function(e) {
-        var html = '';
+        var html = '';// + i + '<br/>';
         var id = $(this).children('a[href]').attr('href');
         $('#' + id + ' div.form-item').each(function() {
           var item = $(this);
-          var text = $('input[type~="text"]', item);
-          if(text.length) {
-            var id = text.attr('id');
-            var label = $('label[for=' + id + ']', item);
-            if(label.length) {
+          $('> input[type~="text"]', item).each(function(i, text) {
+            var id = $(text).attr('id');
+            var label = $('label[for="' + id + '"]', item);
+            if(label.length > 0) {
               label = label.text();
-              text = text.val();
-              $('input[class~="form-tag"]', item).each(function() {
+              var textOut = $(text).val();
+              $('input[class~="form-tag"]', $(text).parent()).each(function() {
                 var tag = $(this);
-                text += ' ' + tag.val();
+                textOut += ' ' + tag.val();
               });
-              text = jQuery.trim(text);
-              if(text != "") {
-                html += label + ' ' + text + '<br/>';
+              textOut = jQuery.trim(textOut);
+              if(textOut.length > 0) {
+                html += label + ' ' + textOut + '<br/>';
               }
             }
-          }
-          var select = $('select', item);
-          if(select) {
-            var id = select.attr('id');
-            var label = $('label[for=' + id + ']', item);
-            if(label.length) {
-              label = label.text();
+          });
+          
+          $('> select', item).each(function(index, select) {
+            var id = $(select).attr('id');
+            var label = $('label[for=' + id + ']');
+            if(label.length > 0) {
+              label = label.text().trim();
               html += label + ' ';
             }
-            var selected = $('option:selected', select);
-            html += selected.text(); + '<br/>';
-          }
+            $('option:selected', select).each(function(idx, selected) {
+              html += $(selected).text().trim() + '<br/>';
+            });
+          });
         });
         html = jQuery.trim(html);
         if(html == "") {
           html = "Empty";
         }
         
-        tip.html(html);
+        if (Drupal.settings.xml_form_elements.tabpanel.tool_tip != null) {
+          Drupal.settings.xml_form_elements.tabpanel.tool_tip.remove();
+        }
+        else {
+          Drupal.settings.xml_form_elements.tabpanel.tool_tip = $(document.createElement('span')).addClass('tool_tip');
+        }
+        
+        Drupal.settings.xml_form_elements.tabpanel.tool_tip.html(html);
         
         var x = e.pageX + 20,
           y = e.pageY + 20,
-          w = tip.width(),
-          h = tip.height(),
+          w = Drupal.settings.xml_form_elements.tabpanel.tool_tip.width(),
+          h = Drupal.settings.xml_form_elements.tabpanel.tool_tip.height(),
           dx = $(window).width() - (x + w),
           dy = $(window).height() - (y + h);
         if ( dx < 20 ) x = e.pageX - w - 20;
         if ( dy < 20 ) y = e.pageY - h - 20;
-        tip.css({
+        Drupal.settings.xml_form_elements.tabpanel.tool_tip.css({
           'left': x, 
           'top': y
         });
         
-        tip.appendTo('body');
-        Drupal.settings.xml_form_elements.tabpanel.tool_tip = tip;
+        Drupal.settings.xml_form_elements.tabpanel.tool_tip.appendTo('body');
       },
       function() {
-        tip.remove();
+        if(Drupal.settings.xml_form_elements.tabpanel.tool_tip != null) {
+          Drupal.settings.xml_form_elements.tabpanel.tool_tip.remove();
+        }
       });      
     });
   },
@@ -133,7 +136,6 @@ Drupal.settings.xml_form_elements.tabpanel = {
       $("#"+id).trigger("mousedown");
       if (Drupal.settings.xml_form_elements.tabpanel.tool_tip != null) {
         Drupal.settings.xml_form_elements.tabpanel.tool_tip.remove();
-        Drupal.settings.xml_form_elements.tabpanel.tool_tip = null;
       }
     });
   },
@@ -142,7 +144,6 @@ Drupal.settings.xml_form_elements.tabpanel = {
     
     if (Drupal.settings.xml_form_elements.tabpanel.tool_tip != null) {
       Drupal.settings.xml_form_elements.tabpanel.tool_tip.remove();
-      Drupal.settings.xml_form_elements.tabpanel.tool_tip = null;
     }
     return false;
   }
@@ -155,6 +156,10 @@ $(document).ready(function() {
   Drupal.settings.xml_form_elements.tabpanel.loadPanels(true);
   Drupal.settings.xml_form_elements.tabpanel.enableActions();
   $("body").ajaxComplete(function(event, request, settings) {
+    if (Drupal.settings.xml_form_elements.tabpanel.tool_tip != null) {
+      Drupal.settings.xml_form_elements.tabpanel.tool_tip.remove();
+      Drupal.settings.xml_form_elements.tabpanel.tool_tip = null;
+    }
     Drupal.settings.xml_form_elements.tabpanel.loadPanels(false);
   });
 });
