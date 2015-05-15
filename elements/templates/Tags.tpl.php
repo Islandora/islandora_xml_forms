20c82f84367a740e9ea2a1c3cdcb9f5b96daebc2
Jared Whiklo <jwhiklo@gmail.com>
2015-05-15 16:03:54 -0500

    Runs tag values through check_plain to keep them from breaking with double-quotes, un-escape for display.

--- elements/templates/Tags.tpl.php
+++ elements/templates/Tags.tpl.php
@@ -21,7 +21,7 @@
   <span class="tag-list">
     <?php foreach ($tags as $tag): ?>
       <span title="<?php print "{$tag['#value']}" ?>">
-        <span class="edit-tag" onclick="jQuery('#<?php print $edit[$tag['#hash']]['#id'] ?>').trigger('mousedown'); return false;"><?php print "{$tag['#value']}" ?></span>
+        <span class="edit-tag" onclick="jQuery('#<?php print $edit[$tag['#hash']]['#id'] ?>').trigger('mousedown'); return false;"><?php print decode_entities("{$tag['#value']}") ?></span>
         <span class="remove-tag" onclick="jQuery('#<?php print $remove[$tag['#hash']]['#id'] ?>').trigger('mousedown'); return false;"></span>
       </span>
     <?php endforeach ?>
