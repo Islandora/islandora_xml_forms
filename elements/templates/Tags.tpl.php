fb85f1fe3c3cf01d11d5f3e79b9e96518f7787f6
knjackson30974 <kylea.jackson4@gmail.com>
2016-01-27 12:20:03 -0400

    Adding check so that empty tags are not printed in new tabs.

--- elements/templates/Tags.tpl.php
+++ elements/templates/Tags.tpl.php
@@ -8,7 +8,7 @@
   <?php print $textfield; ?>
   <input type="image" src="<?php print $image_path ?>add.png" name="<?php print $add['#name'] ?>" id="<?php print $add['#id'] ?>" value="add">
   <div class="hidden-controls">
-    <?php 
+    <?php
       print ((is_array($edit) && array_key_exists('#children', $edit)) ? $edit['#children'] : '');
       print ((is_array($remove) && array_key_exists('#children', $remove)) ? $remove['#children'] : '');
   ?>
@@ -20,10 +20,12 @@
   </div>
   <span class="tag-list">
     <?php foreach ($tags as $tag): ?>
-      <span title="<?php print "{$tag['#value']}" ?>">
-        <span class="edit-tag" onclick="jQuery('#<?php print $edit[$tag['#hash']]['#id'] ?>').trigger('mousedown'); return false;"><?php print decode_entities("{$tag['#value']}") ?></span>
-        <span class="remove-tag" onclick="jQuery('#<?php print $remove[$tag['#hash']]['#id'] ?>').trigger('mousedown'); return false;"></span>
-      </span>
+      <?php if (!empty($tag['#value'])): ?>
+        <span title="<?php print "{$tag['#value']}" ?>">
+          <span class="edit-tag" onclick="jQuery('#<?php print $edit[$tag['#hash']]['#id'] ?>').trigger('mousedown'); return false;"><?php print decode_entities("{$tag['#value']}") ?></span>
+          <span class="remove-tag" onclick="jQuery('#<?php print $remove[$tag['#hash']]['#id'] ?>').trigger('mousedown'); return false;"></span>
+        </span>
+      <?php endif; ?>
     <?php endforeach ?>
   </span>
 </div>
